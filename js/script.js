fa4d050026f3b709d3d84588bee7088448f856dd
juliasilva00 <juliacajuru@gmail.com>
2021-08-26 14:17:44 -0300

    commit atualizacao final do site

---
+++ js/script.js
@@ -0,0 +1,22 @@
+if(window.SimpleSlide){
+    new SimpleSlide({
+        slide: "quote", // nome do atributo data-slide="principal"
+        time: 5000, // tempo de transição dos slides
+        pauseOnHover: true, // pausa a transição automática
+      });
+    
+      new SimpleSlide({
+        slide: "portifolio", 
+        nav: true,
+        time: 5000, 
+        pauseOnHover: true, 
+      });
+
+}
+
+if(window.SimpleAnime){
+    new SimpleAnime();
+}
+
+
+  
